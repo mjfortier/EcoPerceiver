bdd38f08d27cbba6ed16651b2db77a772237177a
Matthew Fortier <fortier.matt@gmail.com>
2025-08-25 08:31:18 -0600

    le to flux targets

--- ecoperceiver/constants.py
+++ ecoperceiver/constants.py
@@ -1,8 +1,8 @@
 EC_PREDICTORS = ('DOY', 'TOD', 'TA', 'P', 'RH', 'VPD', 'PA', 'CO2', 'SW_IN', 'SW_IN_POT', 'SW_OUT', 'LW_IN', 'LW_OUT',
                  'NETRAD', 'PPFD_IN', 'PPFD_OUT', 'WS', 'WD', 'USTAR', 'SWC_1', 'SWC_2', 'SWC_3', 'SWC_4', 'SWC_5',
-                 'TS_1', 'TS_2', 'TS_3', 'TS_4', 'TS_5', 'WTD', 'G', 'H', 'LE')
+                 'TS_1', 'TS_2', 'TS_3', 'TS_4', 'TS_5', 'WTD', 'G', 'H')
 
-EC_TARGETS = ('NEE', 'GPP_DT', 'GPP_NT', 'RECO_DT', 'RECO_NT', 'FCH4')
+EC_TARGETS = ('NEE', 'GPP_DT', 'GPP_NT', 'RECO_DT', 'RECO_NT', 'FCH4', 'LE')
 
 GEO_PREDICTORS = ('lat', 'lon', 'elev')
 
