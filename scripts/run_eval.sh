09a134ea28193494ae8a662cda4be19bd75d65d4
Luis Lara <luis.lara@mila.quebec>
2026-02-26 14:33:27 -0500

    run_eval.sh updated

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:h100:4
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=8
-#SBATCH --time=03:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/eval.out
 #SBATCH --error=logs/eval.error
 #SBATCH --job-name=eval
@@ -14,10 +14,11 @@ cd ~/links/scratch/EcoPerceiver
 RUN_PATH="experiments/runs/final_v2_3e-06_ws_l128_f12_e32_c32_o0.3_wcswcswcswcsssss_CC/seed_0"
 
 PYTHONPATH=. python eval/test_sites.py \
-    --run_folder "$RUN_PATH" \
-    --checkpoint_path checkpoint-11.pth \
-    --batch-size 1024 \
-    --num_workers 8 \
+    --run-path "$RUN_PATH" \
+    --checkpoint-path checkpoint-11.pth \
+    --batch-size 2048 \
+    --num-workers 8 \
+    --data-dir "/home/l/luislara/links/projects/aip-pal/luislara/carbonsense_v2" 
 
 PYTHONPATH=. python eval/export_latex_tables.py \
-    --run_folder "$RUN_FOLDER"
+    --run_folder "$RUN_PATH"
